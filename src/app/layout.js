e275f2441f6880840dcf604bda8575f01895db59
nianer-huaixu <1836829002@qq.com>
2025-03-14 17:30:48 +0800

    项目公共组件初始化

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Geist, Geist_Mono } from "next/font/google"
+import Header from "@/components/Header"
+import Footer from "@/components/Footer";
+
+import "../styles/globals.scss"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,11 +21,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <html lang="ch">
+      <head>
+        <title>江苏扬东铝业集团-高精工业铝板|铝棒|铝卷|铝管|铝型材|航空铝|进口铝</title>
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Header/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,14 @@
-import Image from "next/image";
+import React from "react";
+import Image from "next/image"
+import * as motion from "motion/react-client"
 
 export default function Home() {
+  const box = {
+    width: 100,
+    height: 100,
+    backgroundColor: "#ff0088",
+    borderRadius: 5,
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -24,7 +32,12 @@ export default function Home() {
             Save and see your changes instantly.
           </li>
         </ol>
-
+        <motion.div 
+          style={box}
+          animate={{ rotate: 360 }}
+          transition={{ duration: 1 }}
+        />
+        
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
